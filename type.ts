cfe1f26965b8d62fb299bc106de5724a96a6c63c
ManiaciaChao <maniaciachao@gmail.com>
2019-11-13 20:05:46 +0800

    :construction: ((type)) add schema for author and paper

--- type.ts
+++ type.ts
@@ -12,3 +12,56 @@ export interface ICategory {
   href: string;
   area: string;
 }
+
+export interface IAuthor {
+  id: string;
+  name: string;
+  normailized_name: string;
+    /**
+   * @description affiliations
+   */
+  orgs: string[];
+      /**
+   * @description last known affiliations
+   */
+  org:string;
+  position: string[];
+  /**
+   * @description the number of author publications
+   */
+  n_pubs: number;
+  /**
+   * @description citation count
+   */
+  n_citation: number;
+  h_index: number;
+  tags: {
+    /**
+     * @description research interests
+     */
+    t: string;
+    /**
+     * @description weight of interests
+     */
+    w: number;
+  };
+  pubs: {
+    /**
+     * @description paper id
+     */
+    i: string;
+    /**
+     * @description author order in the paper
+     */
+    r: number;
+  };
+}
+
+export interface IPaper {
+  id:string;
+  n_citation:number;
+  authors:{
+    name:string;
+    id:string;
+  }[];
+}
